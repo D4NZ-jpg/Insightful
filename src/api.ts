f283506aee8d1c055d75054e14264b34a08ae61f
Dan <57308957+D4NZ-jpg@users.noreply.github.com>
2024-01-27 14:20:54 -0600

    feat: add api.ts for API interaction abstraction
    - Implement api.ts to abstract and handle API interactions

---
+++ src/api.ts
@@ -0,0 +1,63 @@
+import { request } from "obsidian";
+
+interface Embedding {
+	index: number;
+	object: string;
+	embedding: Array<number>;
+}
+
+interface APIResponse {
+	id: string;
+	object: string;
+	created: number;
+	model: string;
+	data: Array<Embedding>;
+}
+
+interface EmbeddingRequest {
+	model: string;
+	input: string;
+}
+
+export default class APIWrapper {
+	private apiKey: string;
+	private baseUrl: string;
+
+	constructor(apiKey: string, baseUrl: string) {
+		this.apiKey = apiKey;
+		this.baseUrl = baseUrl;
+	}
+
+	async makeRequest(endpoint: string, data: EmbeddingRequest, method: string): Promise<APIResponse> {
+		const url = `${this.baseUrl}/${endpoint}`;
+
+		try {
+
+			const req = {
+				url: url,
+				method: method,
+				headers: {
+					'Authorization': `Bearer ${this.apiKey}`,
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(data)
+			};
+
+			const response = JSON.parse(await request(req));
+
+			return await response as APIResponse;
+
+		} catch (error) {
+			console.error('API request error:', error);
+			throw error;
+		}
+	}
+
+	async getEmbeddings(model: string, inputText: string): Promise<APIResponse> {
+		const data: EmbeddingRequest = {
+			model: model,
+			input: inputText
+		};
+		return await this.makeRequest('embeddings', data, 'POST');
+	}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
 interface InsighfulSettings {
 	host: string;
+	api_key: string;
+	embedding_model: string;
 }
 
 const DEFAULT_SETTINGS: InsighfulSettings = {
-	host: 'https://api.openai.com'
+	host: 'https://api.openai.com',
+	api_key: '',
+	embedding_model: 'text-embedding-3-small'
 }
 
 export default class Insightful extends Plugin {
@@ -16,7 +18,6 @@ export default class Insightful extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
@@ -55,6 +56,31 @@ class SettingTab extends PluginSettingTab {
 				.setValue(this.plugin.settings.host)
 				.onChange(async (value) => {
 					this.plugin.settings.host = value.trim();
+					await this.plugin.saveSettings();
+				}));
+
+		// Embedding model
+		new Setting(containerEl)
+			.setName('Embedding model')
+			.setDesc('Embedding model to be used')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.embedding_model)
+				.onChange(async (value) => {
+					this.plugin.settings.embedding_model = value.trim().toLowerCase();
+					await this.plugin.saveSettings();
+				}));
+
+
+		// Embedding model
+		new Setting(containerEl)
+			.setName('API Key')
+			.setDesc('API Key to use (if needed)')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.api_key)
+				.onChange(async (value) => {
+					this.plugin.settings.api_key = value.trim();
 					await this.plugin.saveSettings();
 				}));
 	}
